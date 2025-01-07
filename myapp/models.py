97af2cdb147c9e78f7f70f226fe87380e2dac4d2
BrianT5702 <chin-@graduate.utm.my>
2025-01-07 12:59:24 +0800

    Added models for Project and Wall

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Wall(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='walls')
+    start_x = models.FloatField()
+    start_y = models.FloatField()
+    end_x = models.FloatField()
+    end_y = models.FloatField()
+    thickness = models.FloatField(default=0.2)
